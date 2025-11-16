0d0526a2770affd8e8bbcc25dfcb7806edc61a32
jeeby23 <shola.mujeeb@gmail.com>
2025-11-16 16:17:28 +0100

    create footer section

--- src/screens/HomePage.vue
+++ src/screens/HomePage.vue
@@ -1,5 +1,6 @@
 <script setup lang="ts">
 import HeroSection from '@/components/organisms/HeroSection.vue'
+import MainFooter from '@/components/organisms/MainFooter.vue';
 import MainHeader from '@/components/organisms/MainHeader.vue'
 </script>
 
@@ -7,5 +8,6 @@ import MainHeader from '@/components/organisms/MainHeader.vue'
   <div class="flex min-h-screen flex-col gap-6">
     <MainHeader />
     <HeroSection />
+    <MainFooter/>
   </div>
 </template>
