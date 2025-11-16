3ba80a2a79b474d796048711e415ffd782218e33
Kudaru Deborah Nshuk <deborahkudaru@gmail.com>
2025-11-16 22:45:51 +0100

    fix: make path align with watchlist page

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,13 +1,19 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import HomePage from '@/screens/HomePage.vue'
+// import HomePage from '@/screens/HomePage.vue'
+import WailistPage from '@/screens/WailistPage.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
-    {
+    // {
+    //   path: '/',
+    //   name: 'home',
+    //   component: HomePage,
+    // },
+        {
       path: '/',
-      name: 'home',
-      component: HomePage,
+      name: 'waitlist',
+      component: WailistPage,
     },
   ],
   scrollBehavior: () => ({ top: 0 }),
---
+++ src/screens/WailistPage.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+import WaitlistSection from '@/components/organisms/WaitlistSection.vue';
+import MainHeader from '@/components/organisms/MainHeader.vue'
+</script>
+
+<template>
+  <div class="flex min-h-screen flex-col gap-6">
+    <MainHeader />
+     <WaitlistSection />
+    <!-- <MainFooter/> -->
+  </div>
+</template>
