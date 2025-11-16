40a6c22af948042df6d3af3059fefa945e6c4dcb
ALADETAN-IFE <famousfotune123@gmail.com>
2025-11-16 15:50:20 +0100

    feat: add DashboardPreview component with preview image

--- src/screens/HomePage.vue
+++ src/screens/HomePage.vue
@@ -1,11 +1,13 @@
 <script setup lang="ts">
 import HeroSection from '@/components/organisms/HeroSection.vue'
 import MainHeader from '@/components/organisms/MainHeader.vue'
+import DashboardPreview from '@/components/organisms/DashboardPreview.vue'
 </script>
 
 <template>
   <div class="flex min-h-screen flex-col gap-6">
     <MainHeader />
     <HeroSection />
+    <DashboardPreview />
   </div>
 </template>
