e63d43c94fdf65bd46a353145ae7c6c12b7c045f
Kudaru Deborah Nshuk <deborahkudaru@gmail.com>
2025-11-17 06:23:02 +0100

    feat: create complete pc view waitlist page

--- src/components/organisms/WaitlistSection.vue
+++ src/components/organisms/WaitlistSection.vue
@@ -7,9 +7,7 @@ import PillTag from '@/components/atoms/PillTag.vue'
 
 <template>
   <section class="relative flex w-full flex-1 items-center justify-center pt-4 pb-8 sm:pt-6 sm:pb-16">
-    <div
-      class="mx-auto flex w-full items-center max-w-[900px] flex-col gap-6 px-6 py-12 text-center sm:px-8 sm:py-16"
-    >
+    <div class="mx-auto flex w-full items-center max-w-[900px] flex-col gap-6 px-6 pt-12  text-center sm:px-8 sm:py-16">
       <PillTag>Join the Waitlist</PillTag>
 
       <HeadingText tag="h1" align="center" class="text-4xl sm:text-5xl lg:text-5xl font-bold leading-tight">
@@ -23,23 +21,21 @@ import PillTag from '@/components/atoms/PillTag.vue'
       </BodyText>
 
       <div class="flex flex-col sm:flex-row items-center gap-3 w-full max-w-[520px] mt-2">
-        <input
-          type="email"
-          placeholder="Enter Email"
-          class="flex-1 w-full px-4 py-3 text-base border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-transparent"
-        />
+        <input type="email" placeholder="Enter Email"
+          class="flex-1 w-full px-4 py-3 text-base border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-transparent" />
         <BaseButton class="w-full sm:w-auto whitespace-nowrap px-6" size="lg" variant="primary">
           Join the Waitlist
         </BaseButton>
       </div>
+      <p class="text-xs text-[#6A7282]">We respect your privacy. No spam, Unsuscribe anytime.</p>
 
-      <div class="mt-8 w-full max-w-[800px]">
-        <img 
-          src="/images/dashboard.png" 
-          alt="Legal WatchDog Dashboard" 
+      <!-- <div class="mt-8 w-full max-w-[800px]">
+        <img
+          src="/images/dashboard.png"
+          alt="Legal WatchDog Dashboard"
           class="w-full h-auto rounded-lg shadow-2xl"
         />
-      </div>
+      </div> -->
 
     </div>
   </section>
--- src/screens/WailistPage.vue
+++ src/screens/WailistPage.vue
@@ -1,12 +1,15 @@
 <script setup lang="ts">
 import WaitlistSection from '@/components/organisms/WaitlistSection.vue';
 import MainHeader from '@/components/organisms/MainHeader.vue'
+import DashboardPreview from '@/components/organisms/DashboardPreview.vue';
+import MainFooter from '@/components/organisms/MainFooter.vue';
 </script>
 
 <template>
   <div class="flex min-h-screen flex-col gap-6">
     <MainHeader />
      <WaitlistSection />
-    <!-- <MainFooter/> -->
+     <DashboardPreview />
+    <MainFooter/>
   </div>
 </template>
