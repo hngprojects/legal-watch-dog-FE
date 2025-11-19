54e4b3c5e950aa500a054b37e06fb6c42028d557
Oluwaseyi Adisa <oluwaseyiadisaa@gmail.com>
2025-11-19 03:27:39 +0100

    chore: move auth types to src/feature/auth/type.ts
    Moved the auth types to the specified folder as documented in the project specification

---
+++ src/features/auth/types.ts
@@ -0,0 +1,56 @@
+// src/types/auth.ts
+
+export interface RegisterRequest {
+  organisation_name: string
+  first_name: string
+  last_name: string
+  email: string
+  password: string
+}
+
+export interface RegisterResponse {
+  message: string
+  organisation: {
+    id: string
+    name: string
+    created_at: string // ISO date string
+  }
+  user: {
+    id: string
+    first_name: string
+    last_name: string
+    email: string
+    role: 'ADMIN' | 'USER' // can extend later
+    organisation_id: string
+  }
+  token: string // JWT access token
+}
+
+export interface LoginRequest {
+  email: string
+  password: string
+}
+
+export interface LoginResponse {
+  token: string
+  user: {
+    id: string
+    first_name: string
+    last_name: string
+    email: string
+    role: 'ADMIN' | 'USER'
+    organisation_id: string
+  }
+}
+
+export interface LogoutResponse {
+  message: 'Logged out'
+}
+
+export interface RefreshTokenRequest {
+  refresh_token: string
+}
+
+export interface RefreshTokenResponse {
+  token: string // new access token
+}
