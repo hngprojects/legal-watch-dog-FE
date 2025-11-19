ac13e2102412553a98d1db6f8765fdcad3315c9b
Oluwaseyi Adisa <oluwaseyiadisaa@gmail.com>
2025-11-19 06:31:14 +0100

    config: change vite server port to 3000
    Changed vite to run on port 3000 instead of 5173 because backend implemented CORS only for localhost:3000

--- vite.config.ts
+++ vite.config.ts
@@ -7,15 +7,13 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [
-    vue(),
-    vueJsx(),
-    vueDevTools(),
-    tailwindcss()
-  ],
+  server: {
+    port: 3000,
+  },
+  plugins: [vue(), vueJsx(), vueDevTools(), tailwindcss()],
   resolve: {
     alias: {
-     '@': resolve(__dirname, './src'),
+      '@': resolve(__dirname, './src'),
     },
   },
 })
