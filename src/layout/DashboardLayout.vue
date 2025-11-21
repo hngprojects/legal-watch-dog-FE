d7ff1cd748f0e6edf9c42254d8085d2fe7e0d4b0
Kudaru Deborah Nshuk <deborahkudaru@gmail.com>
2025-11-21 23:33:16 +0100

    fix: remove sidebar from dashboard layout

--- src/layout/DashboardLayout.vue
+++ src/layout/DashboardLayout.vue
@@ -5,7 +5,6 @@ import Sidebar from '../components/dashboard/Sidebar.vue'
 
 <template>
   <div class="flex h-screen overflow-hidden bg-gray-50">
-    <Sidebar />
     <div class="flex min-w-0 flex-1 flex-col overflow-hidden">
       <Navbar />
       <main class="flex-1 overflow-y-auto p-6">
