a2bf5fdbcb74d7fb4012f4ad0c0d1d58c081fe9f
Oluwaseyi Adisa <oluwaseyiadisaa@gmail.com>
2025-11-26 00:49:11 +0100

    chore: remove dark mode styles in shadcn button

--- src/components/ui/button/index.ts
+++ src/components/ui/button/index.ts
@@ -4,17 +4,16 @@ import { cva } from 'class-variance-authority'
 export { default as Button } from './Button.vue'
 
 export const buttonVariants = cva(
-  "inline-flex items-center justify-center gap-2 whitespace-nowrap rounded-md text-sm font-medium transition-all disabled:pointer-events-none disabled:opacity-50 [&_svg]:pointer-events-none [&_svg:not([class*='size-'])]:size-4 shrink-0 [&_svg]:shrink-0 outline-none focus-visible:border-ring focus-visible:ring-ring/50 focus-visible:ring-[3px] aria-invalid:ring-destructive/20 dark:aria-invalid:ring-destructive/40 aria-invalid:border-destructive cursor-pointer",
+  "inline-flex items-center justify-center gap-2 whitespace-nowrap rounded-md text-sm font-medium transition-all disabled:pointer-events-none disabled:opacity-50 [&_svg]:pointer-events-none [&_svg:not([class*='size-'])]:size-4 shrink-0 [&_svg]:shrink-0 outline-none focus-visible:border-ring focus-visible:ring-ring/50 focus-visible:ring-[3px] aria-invalid:ring-destructive/20 aria-invalid:border-destructive cursor-pointer",
   {
     variants: {
       variant: {
         default: 'bg-accent-main text-foreground hover:bg-primary/90',
         destructive:
-          'bg-destructive text-white hover:bg-destructive/90 focus-visible:ring-destructive/20 dark:focus-visible:ring-destructive/40 dark:bg-destructive/60',
-        outline:
-          'border bg-surface shadow-xs hover:bg-accent-main hover:text-surface dark:bg-input/30 dark:border-input dark:hover:bg-input/50',
+          'bg-destructive text-white hover:bg-destructive/90 focus-visible:ring-destructive/20',
+        outline: 'border bg-surface shadow-xs hover:bg-accent-main hover:text-surface',
         secondary: 'bg-accent-main text-white hover:bg-accent/50',
-        ghost: 'border border-destructive hover:bg-accent/50 dark:hover:bg-accent/50',
+        ghost: 'border border-destructive hover:bg-accent/50',
         link: 'text-primary underline-offset-4 hover:underline',
       },
       size: {
