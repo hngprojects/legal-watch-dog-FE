2195b896d030c80fa924d4f29e64d0a910f53a96
MKNas01 <nasirumuhammedkabirux@gmail.com>
2025-12-01 14:12:03 +0100

    fix(ui): add spacing between buttons in all SweetAlert2 modals

--- src/lib/swal.ts
+++ src/lib/swal.ts
@@ -4,8 +4,10 @@ const themedSwal = Swal.mixin({
   customClass: {
     confirmButton: 'btn--default btn--lg',
     cancelButton: 'btn--secondary btn--lg',
+    actions: 'gap-4',
   },
   buttonsStyling: false,
+  reverseButtons: true,
 })
 
 export default themedSwal
