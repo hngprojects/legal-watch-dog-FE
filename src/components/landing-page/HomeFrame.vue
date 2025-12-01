b228f744b65f02c7d700b4bf261ce5bb7e6abba2
Kudaru Deborah Nshuk <deborahkudaru@gmail.com>
2025-12-01 17:03:04 +0100

    feat: make contack us, features and how it works pages  responsive

--- src/components/landing-page/HomeFrame.vue
+++ src/components/landing-page/HomeFrame.vue
@@ -1,8 +1,13 @@
 <script setup lang="ts">
 import HomeFrame from '@/assets/Images/HomeFrame.png'
 </script>
+
 <template>
-  <div class="app-container mt-16">
-    <img :src="HomeFrame" alt="Home-Frame-image" class="mx-auto w-full max-w-[1242px]" />
+  <div class="app-container mt-8 px-4 sm:mt-12 sm:px-6 lg:mt-16">
+    <img 
+      :src="HomeFrame" 
+      alt="Home-Frame-image" 
+      class="mx-auto h-auto w-full max-w-full rounded-lg shadow-lg sm:max-w-[900px] sm:rounded-xl lg:max-w-[1242px] lg:rounded-2xl" 
+    />
   </div>
 </template>
