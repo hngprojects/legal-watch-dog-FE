922bccf0375f84798038f8552d4380d5dc60f617
ALADETAN-IFE <famousfotune123@gmail.com>
2025-12-05 13:30:45 +0100

    feat: add warning icon and update expired token display

--- src/views/dashboard/apiAcess/TokenItem.vue
+++ src/views/dashboard/apiAcess/TokenItem.vue
@@ -1,5 +1,6 @@
 <script setup lang="ts">
 import { Button } from '@/components/ui/button'
+import WarningIcon from '@/assets/icons/warning.svg'
 
 interface Props {
   name: string
@@ -42,18 +43,7 @@ const getInitials = (name: string) => {
           </template>
 
           <div v-if="isExpired" class="flex items-center gap-1.5 text-sm">
-            <svg
-              class="h-4 w-4 text-[#F59E0B]"
-              fill="currentColor"
-              viewBox="0 0 20 20"
-              xmlns="http://www.w3.org/2000/svg"
-            >
-              <path
-                fill-rule="evenodd"
-                d="M8.257 3.099c.765-1.36 2.722-1.36 3.486 0l5.58 9.92c.75 1.334-.213 2.98-1.742 2.98H4.42c-1.53 0-2.493-1.646-1.743-2.98l5.58-9.92zM11 13a1 1 0 11-2 0 1 1 0 012 0zm-1-8a1 1 0 00-1 1v3a1 1 0 002 0V6a1 1 0 00-1-1z"
-                clip-rule="evenodd"
-              />
-            </svg>
+            <img :src="WarningIcon" alt="warning" class="h-4 w-4" />
             <span class="text-[#BF6A02] underline">This token has expired</span>
           </div>
         </div>
@@ -62,7 +52,7 @@ const getInitials = (name: string) => {
 
     <Button
       variant="outline"
-      class="border-[#FCA5A5] whitespace-nowrap text-[#DC2626] hover:bg-[#FEE2E2] hover:text-[#DC2626]"
+      class="border-[#DC2626] hover:border-[#DC2626] bg-[#F7F7F7] hover:bg-[#F7F7F7] text-[#DC2626] hover:text-[#DC2626]"
     >
       Delete
     </Button>
